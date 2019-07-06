8f50d1691dcfb54f54639b858cfea87188bf3cc4
Priyanka Saini <priya.saini6616@gmail.com>
2019-07-06 14:09:59 +0530

    list restaurants and adding restaurants feature added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddrestaurantComponent } from './addrestaurant/addrestaurant.component';
+import { ListrestaurantComponent } from './listrestaurant/listrestaurant.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomepageComponent,
+    pathMatch:'full'
+  },
+  {
+    path:"addRestaurant",
+    component:AddrestaurantComponent
+  },
+  {
+    path:"listRestaurant",
+    component:ListrestaurantComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
